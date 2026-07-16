b821d1c6a0e9af4748fd858f03fa5d51bf7092d4
kittitach1526 <fanta.kittitach@gmail.com>
2026-07-16 16:58:19 +0700

    updatE_url_api

--- app/register.ts
+++ app/register.ts
@@ -1,6 +1,6 @@
 "use server";
 
-const API_BASE_URL = process.env.API_BASE_URL || "http://127.0.0.1:5000";
+const API_BASE_URL = process.env.API_BASE_URL || "https://partner369.pythonanywhere.com";
 
 const ROOT_USERNAME = "root";
 const ROOT_PASSWORD = "p@ssw0rd";
